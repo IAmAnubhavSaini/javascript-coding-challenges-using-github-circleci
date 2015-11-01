bfbbbdf235a72be81d4a0f3d04fba70d8fb9542e
Anubhav Saini <IAmAnubhavSaini@gmail.com>
2015-11-01 12:36:39 +0530

    I am counting zeros now

--- solution.js
+++ solution.js
@@ -1,8 +1,14 @@
 Solution = (function() {
   return {
     countZeros: function(n) {
-      //your solution goes here
-      return 'missing implementation';
+	var zeros = 0,
+	divisor = 10;
+	if(n < 10) return zeros;
+	while(divisor <= n){
+		zeros += n/divisor;
+		divisor *= 10;
+	}
+      return zeros;
     }    
   }
 }());
